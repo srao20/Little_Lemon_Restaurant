67cccb3de809b201bd2188e53257c1680fe15fcb
srao20 <srao.dev@outlook.com>
2024-12-22 09:37:14 -0800

    Added Nav bar functionality

--- little_lemon/src/Nav.js
+++ little_lemon/src/Nav.js
@@ -1,20 +1,36 @@
+import React, { useEffect, useState } from "react";
 import logo from "./images/logo1.jpg"
 function Nav() {
+    const [openMenu, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!openMenu);
+    }
  return(
     <>
-        <nav className='nav-bar'> 
-            <div className = 'site-logo'> 
-                <img src={logo} alt="logo"></img>
+        <nav className={`nav-bar ${openMenu ? 'open' : ''}`}>
+            <div className = 'site-logo'>
+                <a href="/">
+                    <img src={logo} alt="logo"/>
+                </a>
             </div>
-            <ul className='menu'> 
-                <li className='menu-item'><a href="home.html">Home</a></li>
-                <li className='menu-item'><a href="about.html">About</a></li>
-                <li className='menu-item'><a href="menu.html">menu</a></li>
-                <li className='menu-item'><a href="reservations.html">Reservations</a></li>
-                <li className='menu-item'><a href="orderOnline.html">Order Online</a></li>
-                <li className='menu-item'><a href="login.html">Login</a></li>
+            {/*Mobile Nav Bar */}
+            <div className = "menu-icon" onClick={toggleMenu}>
+                <div className ='bar'></div>
+                <div className ='bar'></div>
+                <div className ='bar'></div>
+
+            </div>
+            <ul className={`menu-links ${openMenu ? 'visible' : ''}`}>
+                <li ><a href="home.html">Home</a></li>
+                <li ><a href="about.html">About</a></li>
+                <li ><a href="menu.html">Menu</a></li>
+                <li ><a href="reservations.html">Reservations</a></li>
+                <li ><a href="orderOnline.html">Order Online</a></li>
+                <li ><a href="login.html">Login</a></li>
             </ul>
         </nav>
+        <span>{10+16}</span>
     </>
  )
 }
